cef64e2084713c1700f7e89339e1f1a446d5751c
Samuel Monteiro <sam.monteiro.192@gmail.com>
2025-04-02 22:32:54 -0300

    Add files via upload

---
+++ Primo.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int num, i = 2, primo = 1;
+    printf("Entrada:\n");
+    scanf("%d", &num);
+    while (i < num && primo == 1){
+        if (num % 1 ==0){
+            primo = 0
+        }
+        i++;
+    }
+    if(primo ==1){
+        printf
+    }
+    
+    
+    scanf("%c");
+    return 0;
+}
